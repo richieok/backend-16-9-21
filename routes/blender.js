f48444eac1801a2a43cd8013371a9fade9594143
richieok <richie.okoro@gmail.com>
2021-09-16 14:43:54 +0100

    connected to db

--- routes/blender.js
+++ routes/blender.js
@@ -1,14 +1,20 @@
 const Router = require('express').Router();
+const init = require('../lib/db');
 
-Router.get('/', (req, res)=>{
-    res.json({
-        message: "Blender"
-    });
+Router.get('/', async (req, res) => {
+    const { collection } = await init();
+    const blenderTips = await collection.find({}).toArray();
+    if (blenderTips) {
+        res.json(blenderTips);
+        return;
+    }
+    res.status(400).json({});
+
 });
 
 Router.get('/search', (req, res)=>{
     console.log(req.query);
-    console.log(req.params);
+    
     res.end();
 });
 
